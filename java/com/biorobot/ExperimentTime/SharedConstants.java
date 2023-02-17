6ab445a795070b7a47bcaa0479958de64f8416b7
StalkerExplorer <105020139+StalkerExplorer@users.noreply.github.com>
2023-02-18 00:59:43 +0300

    Add files via upload

---
+++ java/com/biorobot/ExperimentTime/SharedConstants.java
@@ -0,0 +1,18 @@
+package com.biorobot.ExperimentTime;
+
+public interface SharedConstants
+{
+	//массив разметок одной последовательности анимации
+	int[] layoutAdapterPage=new int[]{
+		R.layout.page_0,//эта разметка будет первой в массиве
+		R.layout.page_1,
+		R.layout.page_2,
+	    R.layout.page_3,
+		//это разметки для второй последовательности
+	    R.layout.page_4,
+	    R.layout.page_5,
+	    R.layout.page_6,
+	    R.layout.page_7,
+	};
+
+}
